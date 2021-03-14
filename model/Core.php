55c372916dfd465aaeff85bc0d46f30fa47cd4e2
Samet <99058368@mydavinci.nl>
2021-03-14 17:10:30 +0100

    adding functions

--- model/Core.php
+++ model/Core.php
@@ -1,5 +1,26 @@
 <?php
-// CRUD the SQL
+//Grab DB
+require_once 'Database.php';
 
+
+class CRUB extends Database{
+  // Count amount of Index in DB
+  public function countID(){
+    $sql = "SELECT COUNT(name) FROM characters";
+    $stmt = $this->connection()->query($sql);
+    $AmountIndex = $stmt->fetchColumn();
+    return $AmountIndex;
+  }
+  // Grab all columns of each row
+  public function grabInfo(){
+    $sql = "SELECT * from characters";
+    $stmt = $this->connection()->query($sql);
+    $row = $stmt->fetchAll();
+    return $row;
+  }
+
+
+
+}
 
 ?>
